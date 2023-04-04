4a392f280692027237fe2e0524d99f7a70f514b5
i3nguyen <i3nguyen@uwaterloo.ca>
2023-04-03 21:45:27 -0400

    Updated Shuffle Class

---
+++ shuffle.cc
@@ -0,0 +1,17 @@
+#include "shuffle.h"
+#include<random>
+#include<algorithm>
+#include <chrono>
+
+using namespace std;
+
+Shuffle::Shuffle(vector<int> v):  v{v} {   
+    unsigned gen = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine rng{gen};
+    seed = rng;
+}
+
+int Shuffle::roll () {
+    std::shuffle( v.begin(), v.end(), seed );
+    return v.at(0);
+}
